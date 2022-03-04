6c719f943e1023f7b117211e3c8c6dacc4ecdfbb
qafoori <qafoori.hr@gmail.com>
2022-03-05 01:23:50 +0330

    setup layouts - formatted

--- source/components/layouts/nav-bar/index.tsx
+++ source/components/layouts/nav-bar/index.tsx
@@ -1,10 +1,6 @@
-import { FC } from "react"
+import { FC } from 'react'
 import * as Lib from './lib'
 
 export const NavBarLayout: FC = ({}) => {
-  return (
-    <Lib.S.NavBarContainer>
-      navbar
-    </Lib.S.NavBarContainer>
-  )
+  return <Lib.S.NavBarContainer>navbar</Lib.S.NavBarContainer>
 }
